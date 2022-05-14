081597b87f91ca69e9e5a8bac65847df1b7c213d
Fabio <binhobinho1988@hotmail.com>
2022-05-14 14:45:12 -0300

    full-commit

--- p3/Calculadora.java
+++ p3/Calculadora.java
@@ -3,6 +3,7 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class Calculadora {
 
@@ -12,11 +13,11 @@ public class Calculadora {
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setLocationRelativeTo(null);
         jframe.setResizable(false);
-        jframe.setSize(360, 430);
+        jframe.setSize(143, 216);
         jframe.setTitle("Calculadora");
         jframe.getContentPane().setLayout(new FlowLayout());   
 
-        JLabel lblVisor = new JLabel();        
+        // JLabel lblVisor = new JLabel();        
         
         JButton btn0 = new JButton("0");
         JButton btn1 = new JButton("1");
@@ -33,9 +34,28 @@ public class Calculadora {
         JButton btnSoma = new JButton("+");
         JButton btnSubtracao = new JButton("-");
         JButton btnMultiplicacao = new JButton("x");
-        JButton btnDivisao = new JButton("÷");        
+        JButton btnDivisao = new JButton("÷"); 
         
-        jframe.getContentPane().add(lblVisor);         
+        btn0.setSize(50, 50);
+        btn1.setSize(50, 50);
+        btn2.setSize(50, 50);
+        btn3.setSize(50, 50);
+        btn4.setSize(50, 50);
+        btn5.setSize(50, 50);
+        btn6.setSize(50, 50);
+        btn7.setSize(50, 50);
+        btn8.setSize(50, 50);
+        btn9.setSize(50, 50);
+        btnPonto.setSize(50, 50);
+        btnIgualdade.setSize(50, 50);
+        btnSoma.setSize(50, 50);
+        btnSubtracao.setSize(50, 50);
+        btnMultiplicacao.setSize(50, 50);
+        btnDivisao.setSize(50, 50);
+        
+        
+
+        // jframe.getContentPane().add(lblVisor);         
         jframe.getContentPane().add(btn7);
         jframe.getContentPane().add(btn8);
         jframe.getContentPane().add(btn9);
@@ -53,6 +73,8 @@ public class Calculadora {
         jframe.getContentPane().add(btnIgualdade);
         jframe.getContentPane().add(btnSoma);
         jframe.setVisible(true);
+
+        
 
     }
 
