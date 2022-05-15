061fbfd47f0f159a4cd638f91e0e6a794bc312fd
Fabio <binhobinho1988@hotmail.com>
2022-05-15 12:06:12 -0300

    full-commit

--- p3/Calculadora.java
+++ p3/Calculadora.java
@@ -31,36 +31,61 @@ public class Calculadora {
                 
         JButton btn7 = new JButton("7");
         btn7.setBounds(10, 70, 50, 50);
+        
         JButton btn8 = new JButton("8");
         btn8.setBounds(70, 70, 50, 50);
+        
         JButton btn9 = new JButton("9");
         btn9.setBounds(130, 70, 50, 50);
+        
         JButton btnDivisao = new JButton("÷"); 
         btnDivisao.setBounds(190, 70, 50, 50);
+        btnDivisao.setBackground(color3);
+        btnDivisao.setForeground(color1);
+        
         JButton btn4 = new JButton("4");
         btn4.setBounds(10, 130, 50, 50);
+        
         JButton btn5 = new JButton("5");
         btn5.setBounds(70, 130, 50, 50);
+        
         JButton btn6 = new JButton("6");
         btn6.setBounds(130, 130, 50, 50);
+        
         JButton btnMultiplicacao = new JButton("x");
         btnMultiplicacao.setBounds(190, 130, 50, 50);
+        btnMultiplicacao.setBackground(color3);
+        btnMultiplicacao.setForeground(color1);
+
         JButton btn1 = new JButton("1");
         btn1.setBounds(10, 190, 50, 50);
+        
         JButton btn2 = new JButton("2");
         btn2.setBounds(70, 190, 50, 50);
+        
         JButton btn3 = new JButton("3");
         btn3.setBounds(130, 190, 50, 50);
+        
         JButton btnSubtracao = new JButton("-");
         btnSubtracao.setBounds(190, 190, 50, 50);
+        btnSubtracao.setBackground(color3);
+        btnSubtracao.setForeground(color1);
+
         JButton btn0 = new JButton("0");
         btn0.setBounds(10, 250, 50, 50);
+        
         JButton btnPonto = new JButton(".");
         btnPonto.setBounds(70, 250, 50, 50);
+        
         JButton btnIgualdade = new JButton("=");
         btnIgualdade.setBounds(130, 250, 50, 50);
+        btnDivisao.setBackground(color5);
+        btnDivisao.setForeground(color1);
+        
         JButton btnSoma = new JButton("+");
         btnSoma.setBounds(190, 250, 50, 50);
+        btnSoma.setBackground(color3);
+        btnSoma.setForeground(color1);
 
         jframe.getContentPane().add(lblVisor);         
         jframe.getContentPane().add(btn7);
