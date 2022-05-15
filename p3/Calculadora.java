09d7eebda5b04a0725d8bc031b28d32176de75ab
Fabio <binhobinho1988@hotmail.com>
2022-05-15 12:35:43 -0300

    full-commit

--- p3/Calculadora.java
+++ p3/Calculadora.java
@@ -14,7 +14,7 @@ public class Calculadora {
         Color color4 = new Color(146, 150, 153);
         Color color5 = new Color(229, 97, 74);
 
-        JFrame jframe = new JFrame();               
+        JFrame jframe = new JFrame();
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setLocationRelativeTo(null);
         jframe.setResizable(false);
@@ -31,12 +31,18 @@ public class Calculadora {
                 
         JButton btn7 = new JButton("7");
         btn7.setBounds(10, 70, 50, 50);
+        btn7.setBackground(color4);
+        btn7.setForeground(color1);
         
         JButton btn8 = new JButton("8");
         btn8.setBounds(70, 70, 50, 50);
+        btn8.setBackground(color4);
+        btn8.setForeground(color1);
         
         JButton btn9 = new JButton("9");
         btn9.setBounds(130, 70, 50, 50);
+        btn9.setBackground(color4);
+        btn9.setForeground(color1);
         
         JButton btnDivisao = new JButton("÷"); 
         btnDivisao.setBounds(190, 70, 50, 50);
@@ -45,12 +51,18 @@ public class Calculadora {
         
         JButton btn4 = new JButton("4");
         btn4.setBounds(10, 130, 50, 50);
-        
+        btn4.setBackground(color4);
+        btn4.setForeground(color1);
+
         JButton btn5 = new JButton("5");
         btn5.setBounds(70, 130, 50, 50);
+        btn5.setBackground(color4);
+        btn5.setForeground(color1);
         
         JButton btn6 = new JButton("6");
         btn6.setBounds(130, 130, 50, 50);
+        btn6.setBackground(color4);
+        btn6.setForeground(color1);
         
         JButton btnMultiplicacao = new JButton("x");
         btnMultiplicacao.setBounds(190, 130, 50, 50);
@@ -59,12 +71,18 @@ public class Calculadora {
 
         JButton btn1 = new JButton("1");
         btn1.setBounds(10, 190, 50, 50);
+        btn1.setBackground(color4);
+        btn1.setForeground(color1);
         
         JButton btn2 = new JButton("2");
         btn2.setBounds(70, 190, 50, 50);
+        btn2.setBackground(color4);
+        btn2.setForeground(color1);
         
         JButton btn3 = new JButton("3");
         btn3.setBounds(130, 190, 50, 50);
+        btn3.setBackground(color4);
+        btn3.setForeground(color1);
         
         JButton btnSubtracao = new JButton("-");
         btnSubtracao.setBounds(190, 190, 50, 50);
@@ -73,14 +91,18 @@ public class Calculadora {
 
         JButton btn0 = new JButton("0");
         btn0.setBounds(10, 250, 50, 50);
-        
+        btn0.setBackground(color4);
+        btn0.setForeground(color1);
+
         JButton btnPonto = new JButton(".");
         btnPonto.setBounds(70, 250, 50, 50);
-        
+        btnPonto.setBackground(color4);
+        btnPonto.setForeground(color1);
+
         JButton btnIgualdade = new JButton("=");
         btnIgualdade.setBounds(130, 250, 50, 50);
-        btnDivisao.setBackground(color5);
-        btnDivisao.setForeground(color1);
+        btnIgualdade.setBackground(color5);
+        btnIgualdade.setForeground(color1);
         
         JButton btnSoma = new JButton("+");
         btnSoma.setBounds(190, 250, 50, 50);
