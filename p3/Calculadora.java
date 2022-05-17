41842b8c1887e83bc9717938b796e8a370b86840
Fabio <binhobinho1988@hotmail.com>
2022-05-17 15:07:05 -0300

    full-commit

--- p3/Calculadora.java
+++ p3/Calculadora.java
@@ -15,19 +15,26 @@ import javax.swing.plaf.basic.BasicButtonUI;
 
 public class Calculadora implements MouseListener {
 
-    Color color1 = new Color(255, 255, 255); 
-    Color color2 = new Color(36, 41, 47); 
-    Color color3 = new Color(146, 150, 153); 
-    Color color4 = new Color(82, 183, 227); 
-    Color color5 = new Color(229, 97, 74); 
-    Color color6 = new Color(116, 120, 124); 
-    Color color7 = new Color(34, 159, 213); 
-    Color color8 = new Color(212, 57, 30); 
+    Color color3;
+    Color color4;
+    Color color5;
+    Color color6;
+    Color color7;
+    Color color8;
+    JLabel lblVisor;
     Component component;
     String componentName;
 
     public void createComponents() {
-        Font f1 = new Font("Helvetica", Font.BOLD, 30);
+        Color color1 = new Color(255, 255, 255);
+        Color color2 = new Color(36, 41, 47);
+        color3 = new Color(146, 150, 153);
+        color4 = new Color(82, 183, 227);
+        color5 = new Color(229, 97, 74);
+        color6 = new Color(116, 120, 124);
+        color7 = new Color(34, 159, 213);
+        color8 = new Color(212, 57, 30);
+        Font font1 = new Font("Helvetica", Font.BOLD, 30);
         BasicButtonUI bui = new BasicButtonUI();
         Border emptyBorder = new EmptyBorder(10, 10, 10, 10);
         JButton btn7 = new JButton();
@@ -46,24 +53,24 @@ public class Calculadora implements MouseListener {
         JButton btnPonto = new JButton();
         JButton btnIgual = new JButton();
         JButton btnSoma = new JButton();
-        setJButtonSettings(btn7, 10, 70, 50, 50, color3, color1, f1, "btn7", "7", bui, false, null, this);
-        setJButtonSettings(btn8, 70, 70, 50, 50, color3, color1, f1, "btn8", "8", bui, false, null, this);
-        setJButtonSettings(btn9, 130, 70, 50, 50, color3, color1, f1, "btn9", "9", bui, false, null, this);
-        setJButtonSettings(btnDiv, 190, 70, 50, 50, color4, color1, f1, "btnDiv", "÷", bui, false, null, this);
-        setJButtonSettings(btn4, 10, 130, 50, 50, color3, color1, f1, "btn4", "4", bui, false, null, this);
-        setJButtonSettings(btn5, 70, 130, 50, 50, color3, color1, f1, "btn5", "5", bui, false, null, this);
-        setJButtonSettings(btn6, 130, 130, 50, 50, color3, color1, f1, "btn6", "6", bui, false, null, this);
-        setJButtonSettings(btnMult, 190, 130, 50, 50, color4, color1, f1, "btnMult", "×", bui, false, null, this);
-        setJButtonSettings(btn1, 10, 190, 50, 50, color3, color1, f1, "btn1", "1", bui, false, null, this);
-        setJButtonSettings(btn2, 70, 190, 50, 50, color3, color1, f1, "btn2", "2", bui, false, null, this);
-        setJButtonSettings(btn3, 130, 190, 50, 50, color3, color1, f1, "btn3", "3", bui, false, null, this);
-        setJButtonSettings(btnSub, 190, 190, 50, 50, color4, color1, f1, "btnSub", "−", bui, false, null, this);
-        setJButtonSettings(btn0, 10, 250, 50, 50, color3, color1, f1, "btn0", "0", bui, false, null, this);
-        setJButtonSettings(btnPonto, 70, 250, 50, 50, color3, color1, f1, "btnPonto", ".", bui, false, null, this);
-        setJButtonSettings(btnIgual, 130, 250, 50, 50, color5, color1, f1, "btnIgual", "=", bui, false, null, this);
-        setJButtonSettings(btnSoma, 190, 250, 50, 50, color4, color1, f1, "btnSoma", "+", bui, false, null, this);
-        JLabel lblVisor = new JLabel();
-        setJLabelSettings(lblVisor, 10, 10, 230, 50, true, color4, color1, f1, "lblVisor", "0.", JLabel.RIGHT, emptyBorder);
+        setJButtonSettings(btn7, 10, 70, 50, 50, color3, color1, font1, "btn7", "7", bui, false, null, this);
+        setJButtonSettings(btn8, 70, 70, 50, 50, color3, color1, font1, "btn8", "8", bui, false, null, this);
+        setJButtonSettings(btn9, 130, 70, 50, 50, color3, color1, font1, "btn9", "9", bui, false, null, this);
+        setJButtonSettings(btnDiv, 190, 70, 50, 50, color4, color1, font1, "btnDiv", "÷", bui, false, null, this);
+        setJButtonSettings(btn4, 10, 130, 50, 50, color3, color1, font1, "btn4", "4", bui, false, null, this);
+        setJButtonSettings(btn5, 70, 130, 50, 50, color3, color1, font1, "btn5", "5", bui, false, null, this);
+        setJButtonSettings(btn6, 130, 130, 50, 50, color3, color1, font1, "btn6", "6", bui, false, null, this);
+        setJButtonSettings(btnMult, 190, 130, 50, 50, color4, color1, font1, "btnMult", "×", bui, false, null, this);
+        setJButtonSettings(btn1, 10, 190, 50, 50, color3, color1, font1, "btn1", "1", bui, false, null, this);
+        setJButtonSettings(btn2, 70, 190, 50, 50, color3, color1, font1, "btn2", "2", bui, false, null, this);
+        setJButtonSettings(btn3, 130, 190, 50, 50, color3, color1, font1, "btn3", "3", bui, false, null, this);
+        setJButtonSettings(btnSub, 190, 190, 50, 50, color4, color1, font1, "btnSub", "−", bui, false, null, this);
+        setJButtonSettings(btn0, 10, 250, 50, 50, color3, color1, font1, "btn0", "0", bui, false, null, this);
+        setJButtonSettings(btnPonto, 70, 250, 50, 50, color3, color1, font1, "btnPonto", ".", bui, false, null, this);
+        setJButtonSettings(btnIgual, 130, 250, 50, 50, color5, color1, font1, "btnIgual", "=", bui, false, null, this);
+        setJButtonSettings(btnSoma, 190, 250, 50, 50, color4, color1, font1, "btnSoma", "+", bui, false, null, this);
+        lblVisor = new JLabel();
+        setJLabelSettings(lblVisor, 10, 10, 230, 50, true, color4, color1, font1, "lblVisor", "0.", JLabel.RIGHT, emptyBorder);
         JFrame frame = new JFrame();
         Component co[] = {lblVisor, btn7, btn8, btn9, btnDiv, btn4, btn5, btn6, btnMult, btn1, btn2, btn3, btnSub, btn0, btnPonto, btnIgual, btnSoma};
         setJFrameSettings(frame, 250, 345, JFrame.EXIT_ON_CLOSE, false, "Calculadora", null, color2, co, null, true);
@@ -190,6 +197,58 @@ public class Calculadora implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        component = e.getComponent();
+        componentName = e.getComponent().getName();
+        switch (componentName) {
+            case "btn7":
+                lblVisor.setText("7");
+                break;
+            case "btn8":
+                lblVisor.setText("8");
+                break;
+            case "btn9":
+                lblVisor.setText("9");
+                break;
+            case "btn4":
+                lblVisor.setText("4");
+                break;
+            case "btn5":
+                lblVisor.setText("5");
+                break;
+            case "btn6":
+                lblVisor.setText("6");
+                break;
+            case "btn1":
+                lblVisor.setText("1");
+                break;
+            case "btn2":
+                lblVisor.setText("2");
+                break;
+            case "btn3":
+                lblVisor.setText("3");
+                break;
+            case "btn0":
+                lblVisor.setText("0");
+                break;
+            case "btnPonto":
+                lblVisor.setText(".");
+                break;
+            case "btnDiv":
+                lblVisor.setText("÷");
+                break;
+            case "btnMult":
+                lblVisor.setText("×");
+                break;
+            case "btnSub":
+                lblVisor.setText("−");
+                break;
+            case "btnSoma":
+                lblVisor.setText("+");
+                break;
+            case "btnIgual":
+                lblVisor.setText("=");
+                break;
+        }
     }
 
     public static void main(String[] args) {
